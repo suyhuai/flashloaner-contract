c29fc24723f34389154d9239ab5ab7f383724bb2
苏永怀 <suyonghuai@inmyshow.com>
2021-05-30 22:23:05 +0800

    add Example

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -1,8 +1,8 @@
 pragma solidity >=0.5.0;
 
-import './interfaces/IUniswapV2Pair.sol';
+import '../interfaces/IUniswapV2Pair.sol';
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "./SafeMath.sol";
 
 library UniswapV2Library {
     using SafeMath for uint;
