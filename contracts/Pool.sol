159ba1da6786a74bcc7e4d97a86060dedf09b634
苏永怀 <suyonghuai@inmyshow.com>
2021-05-31 00:41:09 +0800

    dev

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -29,6 +29,15 @@ interface IRouter {
         address to,
         uint deadline
     ) external returns (uint amountA, uint amountB, uint liquidity);
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB);
     function getAmountsIn(uint amountOut, address[] calldata path) external view returns (uint[] memory amounts);
 }
 
@@ -39,20 +48,21 @@ interface IPair {
 
 contract TestArbitrage {
   address constant router = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
-  address constant WETHUSDTPair = 0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852;
-  address constant WETHROOTPair = 0x01f8989c1e556f5c89c7D46786dB98eEAAe82c33;
+  address constant LoanPair = 0x3041CbD36888bECc7bbCBc0045E3B1f144466f5f;//USDC-USDT: LoanToken-RepayToken
+  address constant SwapPair = 0x6591c4BcD6D7A1eb4E537DA8B78676C1576Ba244; // BOND-USDC: 0-1:SwapToken-LoanToken
+  address constant LoanToken0 = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+  address constant RepayToken1 = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+  address constant SwapToken0 = 0x0391D2021f89DC339F60Fff84546EA23E337750f;
   address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-  address constant USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
-  address constant ROOT = 0xCb5f72d37685C3D5aD0bB5F982443BC8FcdF570E;
 
 
-event PrintArg(uint256 amount);
+  event PrintArg(uint256 amount);
   event PrintIdx(uint256 index);
 
   constructor() public {
-      safeApprove(WETH,router,uint(-1));
-      safeApprove(USDT,router,uint(-1));
-      safeApprove(ROOT,router,uint(-1));
+      safeApprove(LoanToken0,router,uint(-1));
+      safeApprove(RepayToken1,router,uint(-1));
+      safeApprove(SwapToken0,router,uint(-1));
   }
   
   function deposit() public payable {
@@ -61,9 +71,9 @@ event PrintArg(uint256 amount);
 
   function go(uint256 loanAmount) external {
     //   uint256 loanAmount = 45000 * 1000000000000000000;
-    IPair(WETHUSDTPair).swap(
-      uint(0),
+    IPair(LoanPair).swap(
       uint(loanAmount),
+      uint(0),
       address(this),
       bytes('not empty')
     );
@@ -71,20 +81,21 @@ event PrintArg(uint256 amount);
 
   function uniswapV2Call(
     address sender,
-    uint _WETHAmount,
-    uint _USDTAmount,
+    uint LoanToken0Amount,
+    uint RepayToken1Amount,
     bytes calldata data
   ) external {
-      emit PrintIdx(0);
-     
+      address[] memory path = new address[](2);
+      path[0] = LoanToken0;
+      path[1] = SwapToken0;
+      
+      uint[] memory amounts1 = IRouter(router).swapExactTokensForTokens(uint(311717017.04323*1000000),uint(0), path, address(this), block.timestamp+1800);
+      uint SwapToken0Amount = amounts1[1];
 
-      address[] memory WETHROOT = new address[](2);
-      WETHROOT[0] = WETH;
-      WETHROOT[1] = ROOT;
-      
-      
-          uint[] memory amounts = IRouter(router).swapExactTokensForTokens(uint(40000*1000000000000000000),uint(0), WETHROOT, address(this), block.timestamp+1800);
-            uint ROOTAmount = amounts[1];
+
+
+      uint[] memory amounts1 = IRouter(router).addLiquidity(SwapToken0, LoanToken0, uint(20000 * 1000000000000000000), uint(0), uint(0), uint(0),IPair(SwapPair), block.timestamp+1800);
+      uint SwapTokenAmount = amounts1[1];
             
       address[] memory WETHUSDT = new address[](2);
       WETHUSDT[0] = WETH;
